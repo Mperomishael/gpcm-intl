105a1a9e5cfdecb4dd3981ec09497b0fa1f2f66b
Mishael Yakubu <147824425+Mperomishael@users.noreply.github.com>
2026-08-09 19:20:52 -1100

    Add routing with BrowserRouter and AdminPage

--- gpcm-app/src/main.tsx
+++ gpcm-app/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App.tsx';
+import AdminPage from './pages/AdminPage.tsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
