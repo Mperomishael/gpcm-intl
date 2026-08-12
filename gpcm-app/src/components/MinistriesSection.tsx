7214ee251fc51b846aee9609be1b29aa777a38f0
Mishael Yakubu <147824425+Mperomishael@users.noreply.github.com>
2026-08-12 11:06:29 +0100

    is

--- gpcm-app/src/components/MinistriesSection.tsx
+++ gpcm-app/src/components/MinistriesSection.tsx
@@ -5,7 +5,7 @@ const ministries = [
     icon: Church,
     title: "Worship",
     description: "Honoring God with joy and reverence in all that we do.",
-    image: "/public/Honoring-God.webp",
+    image: "/Honoring-God.webp",
     verse: "Serve the Lord with gladness...",
     ref: "Psalm 100:2"
   },
