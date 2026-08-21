bdaa47bf13a328887907eeff1856613c7293ed00
Mishael Yakubu <147824425+Mperomishael@users.noreply.github.com>
2026-08-21 03:50:45 +0100

    Change media query to filter by 'published' status

--- gpcm-app/api/media.js
+++ gpcm-app/api/media.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
   const { data, error } = await supabaseAdmin
     .from('media')
     .select('*')
-    .eq('hidden', false)
+    .eq('status', 'published')
     .order('order', { ascending: true });
 
   if (error) return res.status(500).json({ error: error.message });
